685ab4ed196a8be75d4fe419908139a87472da12
saumil92001 <38178854+saumil92001@users.noreply.github.com>
2021-10-04 00:15:57 +0530

    Fixed orientation of app as Portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flashlight/HomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
